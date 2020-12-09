06985dedcec5f9caab3c34eb4e9fe81c3f0a2a8e
方舟 <fangzhou@fangzhoudeMacBook-Pro.local>
2020-12-09 18:15:49 +0800

    first commit

---
+++ assets/draw.js
@@ -0,0 +1,19 @@
+function setup(){
+    createCanvas(800,400);
+    noStroke();
+    colorMode(HSB);
+}
+  
+function draw(){
+    let lefthue = map(mouseX,0,width,0,360);
+    let righthue = (lefthue + 180) % 360;
+    fill(lefthue,100,100);
+    rect(0,0,width/2,height);
+    fill(righthue,100,100);
+    rect(width/2,0,width/2,height);
+  
+}
+function windowResized(){
+    resizeCanvas(windowWidth,windowHeight);
+}
+  
